55efdc6ab286a59ab405b52ae45dd1ceaa3138d9
Slawek84PL <slapyt84@gmail.com>
2024-08-07 22:01:49 +0200

    Implemaentacja funkcjonalności wysyłania maili na podstawie kolelki z RabbitMQ

--- Notification/src/main/java/pl/slawek/notification/model/NotificationService.java
+++ Notification/src/main/java/pl/slawek/notification/model/NotificationService.java
@@ -3,5 +3,6 @@ package pl.slawek.notification.model;
 import pl.slawek.notification.dto.NotificationInfoDto;
 
 public interface NotificationService {
+
     void handleFinishEnroll(NotificationInfoDto notificationInfoDto);
 }
--- Notification/src/main/java/pl/slawek/notification/model/NotificationServiceImpl.java
+++ Notification/src/main/java/pl/slawek/notification/model/NotificationServiceImpl.java
@@ -5,15 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import pl.slawek.notification.dto.NotificationInfoDto;
+import pl.slawek.notification.email.EmailSender;
 
 @AllArgsConstructor
 @Service
 @Slf4j
 public class NotificationServiceImpl implements NotificationService {
 
+    private final EmailSender emailSender;
+
     @Override
     @RabbitListener(queues = "enroll_finish")
     public void handleFinishEnroll(NotificationInfoDto notificationInfo) {
-        log.info(notificationInfo.toString());
+        emailSender.sendEmails(notificationInfo);
     }
 }
